0bbe988164b211a51d4ac3fab4a942b962ae87e3
walid-chihab <walid-chihab@users.noreply.github.com>
2025-04-30 01:40:33 +0100

    subject fixing

--- basic.exe/aff_ascii.cpp
+++ basic.exe/aff_ascii.cpp
@@ -1,3 +1,11 @@
+     /****************************************************************************************************************************/
+    /*	La classe string vous permet d’accéder aux caractères en utilisant soit l’opérateur [] soit la m ́méthode at().	        */
+   /*	Pour connaître la longueur de la chaine, vous pouvez utiliser soit length() soit size().			       */
+  /*	En utilisant la classe string, donner un programme qui affiche les codes ASCII des caractères composant une chaîne de */
+ /*	caractères.											                     */
+/****************************************************************************************************************************/
+
+
 #include <iostream>
 #include <string>
 using namespace std;
