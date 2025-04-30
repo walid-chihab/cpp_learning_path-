44be0c2a15549a1381f0172113406e9ab8a8effc
walid-chihab <walid-chihab@users.noreply.github.com>
2025-04-30 16:56:07 +0100

    essai

---
+++ basic.exe/essai.cpp
@@ -0,0 +1,16 @@
+    #include <stdio.h>
+    using namespace std;
+    
+    struct essai{
+    
+        int n ; float x;
+    
+    }; 
+
+//passage par adresse
+    void miseazero (essai *e1)
+    {
+        e->n = 0;
+        e->x = 0.0;
+        
+    }
