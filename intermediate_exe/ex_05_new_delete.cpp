594bf0df216cb3c8d395281822050fbdebb17e32
walid-chihab <walid-chihab@users.noreply.github.com>
2025-06-03 03:27:42 +0100

    bien fait

--- intermediate_exe/ex_05_new_delete.cpp
+++ intermediate_exe/ex_05_new_delete.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
 using namespace std;
+
 class element {
-	public:
-		
-		int valeur;
-		
-		//constructure fct pas de type de retour
-		element (){
-		valeur = 0 ;
-		}
+public:
+    int valeur;
+
+    // Constructeur par défaut
+    element() {
+        valeur = 0;
+    }
 };
-int main (){
 
-	//creation des objet de la class
-	element e1;//allocation statique   stack
-	element *e2 =new element;//allocation dynamique un seul element 
-	element *tab = new element[3];//aussi dynamique mais trois element	
+int main() {
+    // Allocation statique (stack)
+    element e1;
 
-	//affichage de ces element
-	
-	cout<<"element 1 e1 = "<<e1.valeur<<endl1;
-	cou<<"element 2 e2 =  "<<e2->valeur<<endl1;
-	for (int i = 0 ; i<3 i++){
-		cout<<"t["<<i<<"] = "<< t[i]->valeur<<endl;
-	}
+    // Allocation dynamique d’un seul objet
+    element* e2 = new element;
 
+    // Allocation dynamique d’un tableau de 3 éléments
+    element* tab = new element[3];
 
-	//free de l'espace utiliser
-	// on fait pas delete e1 car elle va libirer directement
-	delete e2;
-	delete[]tab;
+    // Affichage
+    cout << "element 1 e1 = " << e1.valeur << endl;
+    cout << "element 2 e2 = " << e2->valeur << endl;
 
+    for (int i = 0; i < 3; i++) {
+        cout << "tab[" << i << "] = " << tab[i].valeur << endl;
+    }
 
-	return 0;
+    // Libération de la mémoire dynamique
+    delete e2;
+    delete[] tab;
+
+    return 0;
 }
-	
+
