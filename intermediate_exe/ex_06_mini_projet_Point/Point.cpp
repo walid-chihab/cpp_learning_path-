c5d0c83d5ba5b78a88b3c23835907e82c3b87cb9
walid-chihab <walid-chihab@users.noreply.github.com>
2025-06-04 01:58:27 +0100

    teste

--- intermediate_exe/ex_06_mini_projet_Point/Point.cpp
+++ intermediate_exe/ex_06_mini_projet_Point/Point.cpp
@@ -4,10 +4,11 @@
 using namespace std;
 
 //standare valeur par constructeur
-Point :: Point (float a=0 , float b=0) : x(a),y(b) {}
+//preferenr faire ca dans le fichier point.h
+//Point :: Point (float a=0 , float b=0) : x(a),y(b) {}
 
 //standare valeurs par l'utilisateur 
-//Point :: Point (float xval, float yval) : x(xval),y(yval){}
+Point :: Point (float xval, float yval) : x(xval),y(yval){}
  
 //fct deplacer 
 void Point :: deplace (float dx, float dy){
